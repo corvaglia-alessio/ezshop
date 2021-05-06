93590257618ff754d49c0d061000bc33237d6556
Alessio Corvaglia <s281557@studenti.polito.it>
2021-05-06 22:31:51 +0200

    saleTransaction and TicketEntry interfaces implementation

--- src/main/java/it/polito/ezshop/model/SaleTransactionClass.java
+++ src/main/java/it/polito/ezshop/model/SaleTransactionClass.java
@@ -1,7 +1,62 @@
 package it.polito.ezshop.model;
-/*
-public class SaleTransactionClass implements SaleTransaction {
-	
 
+import java.util.ArrayList;
+import java.util.List;
+
+import it.polito.ezshop.data.SaleTransaction;
+import it.polito.ezshop.data.TicketEntry;
+
+public class SaleTransactionClass implements SaleTransaction {
+
+    private Integer ticketNumber;
+    private Double price;
+    private Double discountRate;
+    private List<TicketEntry> entries;
+
+    public SaleTransactionClass(Integer ticketNumber){
+        this.ticketNumber = ticketNumber;
+        this.price = 0D;
+        this.discountRate = 0D;
+        this.entries = new ArrayList<TicketEntry>();
+    }
+
+    @Override
+    public Integer getTicketNumber() {
+        return this.ticketNumber;
+    }
+
+    @Override
+    public void setTicketNumber(Integer ticketNumber) {
+        this.ticketNumber = ticketNumber;
+    }
+
+    @Override
+    public List<TicketEntry> getEntries() {
+        return this.entries;
+    }
+
+    @Override
+    public void setEntries(List<TicketEntry> entries) {
+        this.entries = entries;
+    }
+
+    @Override
+    public double getDiscountRate() {
+        return this.discountRate;
+    }
+
+    @Override
+    public void setDiscountRate(double discountRate) {
+        this.discountRate = discountRate;
+    }
+
+    @Override
+    public double getPrice() {
+        return this.price;
+    }
+
+    @Override
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
-*/
---
+++ src/main/java/it/polito/ezshop/model/TicketEntryClass.java
@@ -0,0 +1,71 @@
+package it.polito.ezshop.model;
+
+import it.polito.ezshop.data.TicketEntry;
+
+public class TicketEntryClass implements TicketEntry {
+
+    private String barCode;
+    private String productDescription;
+    private Integer amount;
+    private Double pricePerUnit;
+    private Double discountRate;
+
+    public TicketEntryClass(String barCode, String productDescription, Integer amount, Double pricePerUnit, Double discountRate){
+        this.barCode = barCode;
+        this.productDescription = productDescription;
+        this.amount = amount;
+        this.discountRate = discountRate;
+        this.pricePerUnit = pricePerUnit;
+    }
+
+    @Override
+    public String getBarCode() {
+        return this.barCode;
+    }
+
+    @Override
+    public void setBarCode(String barCode) {
+        this.barCode = barCode;
+    }
+
+    @Override
+    public String getProductDescription() {
+        return this.productDescription;
+    }
+
+    @Override
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    @Override
+    public int getAmount() {
+        return this.amount;
+    }
+
+    @Override
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public double getPricePerUnit() {
+        return this.pricePerUnit;
+    }
+
+    @Override
+    public void setPricePerUnit(double pricePerUnit) {
+        this.pricePerUnit = pricePerUnit;
+    }
+
+    @Override
+    public double getDiscountRate() {
+       return this.discountRate;
+    }
+
+    @Override
+    public void setDiscountRate(double discountRate) {
+        this.discountRate = discountRate;
+    }
+    
+}
