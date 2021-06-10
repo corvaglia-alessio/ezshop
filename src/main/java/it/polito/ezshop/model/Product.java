23cabee02f083d40f599002725b3465e132f32b5
Federico Mustich <s287345@studenti.polito.it>
2021-06-10 01:13:27 +0000

    Added Utility Methods

--- src/main/java/it/polito/ezshop/model/Product.java
+++ src/main/java/it/polito/ezshop/model/Product.java
@@ -17,4 +17,30 @@ public class Product {
     public Integer getProductId(){
         return this.ProductId;
     }
+
+    private Integer getIntegerRFID(){
+        return Integer.parseInt(this.RFID);
+    }
+
+    private String padToRFID(Integer rfid){
+        String result = rfid.toString();
+
+        if (result.length() > 12){
+            return result.substring(0, 12);
+        }
+        else {
+            int diff = 12 - result.length();
+            String pad = "";
+            for (int i = 0; i < diff; i++){
+                pad += "0";
+            }
+            return pad+result;
+        }
+    }
+
+    public String getRFIDFromOffest(int offset){
+        Integer rfid = this.getIntegerRFID();
+        rfid += offset;
+        return this.padToRFID(rfid);
+    }
 }
